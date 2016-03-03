c50500b8f3798d062c71eddd22016c001e8d2854
Gyanesh Gouraw <gyaneshgouraw@gmail.com>
2016-03-03 20:52:28 +0530

    Basic fixes done

--- public/module1/Controller1.js
+++ public/module1/Controller1.js
@@ -14,7 +14,7 @@ angular.module('ControllerModule', [])
 			$scope.insert.success=false;
 		}
 		function getSuccess(data){
-			document.getElementById("json").innerHTML = JSON.stringify(data.data, undefined, 2)
+			document.getElementById("json").innerHTML = JSON.stringify(data.data.map(function(item){return item.data}), undefined, 2)
 		}
 		function getError(data){
 			console.log(data);
