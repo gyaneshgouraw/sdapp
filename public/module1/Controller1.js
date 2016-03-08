211b7c91320c556416cf809bce6dfb771ac407ea
Gyanesh Gouraw <gyaneshgouraw@gmail.com>
2016-03-08 20:54:09 +0530

    integrated changes for new request

--- public/module1/Controller1.js
+++ public/module1/Controller1.js
@@ -15,6 +15,18 @@ angular.module('ControllerModule', [])
 		}
 		function getSuccess(data){
 			document.getElementById("json").innerHTML = JSON.stringify(data.data.map(function(item){return item.data}), undefined, 2)
+			document.getElementById("output").innerHTML = "";
+			for(var i = 0; i < data.data.length;i++){
+				var newDiv = document.createElement(Object.keys(data.data[i].data).join()); 
+				var newContent = document.createTextNode(data.data[i].data[Object.keys(data.data[i].data).join()]); 
+				newDiv.appendChild(newContent); //add the text node to the newly created div.
+				newDiv.appendChild(document.createElement("hr")); 
+				// add the newly created element and its content into the DOM 
+				var currentDiv = document.getElementById("output"); 
+				currentDiv.appendChild(newDiv);
+			}
+		  
+
 		}
 		function getError(data){
 			console.log(data);
